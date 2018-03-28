890ac6ed75a6d15245f63ae07a9566201e98a2d4
Jonathan Merritt <j.s.merritt@gmail.com>
2018-03-28 22:14:24 +1100

    Parse in stars
    - 117732 successful
    - 1883 (0.99%) failed

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import GHC.Float (double2Float)
-import Data.Text (Text)
-import qualified Data.Text as T
-import qualified Data.Text.Read as T
-import qualified Data.Vector as V
+import           Control.Monad.IO.Class (MonadIO)
+import           Data.Text              (Text)
+import qualified Data.Text              as T
+import qualified Data.Text.Read         as T
+import qualified Data.Vector            as V
+import           GHC.Float              (double2Float)
+import qualified Streamly               as S
+import qualified Streamly.Prelude       as S
+import           System.IO              (IOMode (ReadMode), withFile)
 
 main :: IO ()
-main = putStrLn "Hello World"
+main = do
+    putStrLn "Hello World"
+    inStars <- readStarsFile "data/HYG-Database/hygdata_v3.csv"
+    putStrLn $ concat [ "Number of Stars: ", show . length . inputStarsStars $ inStars ]
+    putStrLn $ concat [ "Number Failed:   ", show . inputStarsFailedCount $ inStars ]
 
 
 data Star
@@ -18,6 +26,29 @@ data Star
     , starMag :: {-# UNPACK #-} !Float
     , starCI  :: {-# UNPACK #-} !Float
     }
+
+
+data InputStars
+    = InputStars
+    { inputStarsStars       :: [Star]
+    , inputStarsFailedCount :: Int
+    }
+    
+
+addInputStar :: InputStars -> Maybe Star -> InputStars
+addInputStar (InputStars stars count) maybeStar =
+    case maybeStar of
+        Just star -> InputStars (star : stars)  count
+        Nothing   -> InputStars         stars  (count + 1)
+    
+
+readStarsFile :: FilePath -> IO InputStars
+readStarsFile filePath = withFile filePath ReadMode $ \handle -> do
+    S.foldl addInputStar (InputStars [] 0) id
+    . S.serially
+    . fmap (readLine . T.pack)
+    . S.fromHandle
+    $ handle
 
 
 readLine :: Text -> Maybe Star
@@ -40,3 +71,12 @@ readLine line =
         <*> lookup 8
         <*> lookup 13
         <*> lookup 16
+
+
+
+-- readStream :: (S.Streaming t) => t m Text ->
+
+{-
+toTextStream :: S.Streaming t => t m String -> t m Text
+toTextStream = fmap T.pack
+-}
