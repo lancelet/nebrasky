89c4beef1fd13d118d808f303744c113004d4606
Jonathan Merritt <j.s.merritt@gmail.com>
2018-03-29 16:51:49 +1100

    Provide a default value for star Color Index
    - 1882 stars did not have a Color Index (CI) value

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import           Control.Monad          (forM_)
 import           Control.Monad.IO.Class (MonadIO)
+import           Data.Maybe             (fromMaybe)
 import           Data.Text              (Text)
 import qualified Data.Text              as T
 import qualified Data.Text.Read         as T
@@ -17,6 +19,9 @@ main = do
     inStars <- readStarsFile "data/HYG-Database/hygdata_v3.csv"
     putStrLn $ concat [ "Number of Stars: ", show . length . inputStarsStars  $ inStars ]
     putStrLn $ concat [ "Number Failed:   ", show . length . inputStarsFailed $ inStars ]
+
+    forM_ (inputStarsFailed inStars) $ \(FailedStarRead txt) ->
+        putStrLn . T.unpack $ txt
 
 
 data Star
@@ -35,14 +40,14 @@ data InputStars
     }
 
 newtype FailedStarRead = FailedStarRead Text
-    
+
 
 addInputStar :: InputStars -> Either FailedStarRead Star -> InputStars
 addInputStar (InputStars stars failed) eitherStar =
     case eitherStar of
         Right star   -> InputStars (star : stars)            failed
         Left notRead -> InputStars         stars  (notRead : failed)
-    
+
 
 readStarsFile :: FilePath -> IO InputStars
 readStarsFile filePath = withFile filePath ReadMode $ \handle -> do
@@ -67,14 +72,14 @@ readLine line =
 
         lookup :: Int -> Maybe Float
         lookup i = items V.!? i >>= toFloat
-    
+
         maybeStar :: Maybe Star
         maybeStar =
             Star
             <$> lookup 7
             <*> lookup 8
             <*> lookup 13
-            <*> lookup 16
+            <*> pure (fromMaybe 0 (lookup 16))
     in
         case maybeStar of
             Nothing   -> Left (FailedStarRead line)
